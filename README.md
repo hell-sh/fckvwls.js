ce0a842616d99f26c6597c8c718e029eab13de2b
Tim Speckhals <timmy14085+github@gmail.com>
2018-04-11 14:09:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# fckvwls.js
+
+Md fr ll y 11-13 yr lds.
+
+- [Xmpl](https://netducks.github.io/fckvwls.js/)
+- [Dwnld](https://raw.githubusercontent.com/netducks/fckvwls.js/master/fckvwls.js)
---
+++ fckvwls.js
@@ -0,0 +1,30 @@
+/* fckvwls.js - Md fr ll y 11-13 yr lds - Cpyrght (c) 2018, Ntdcks */
+
+window.fckvwls = {
+	rmvLlVwls: function()
+	{
+		fckvwls.rmvVwlsFrm(document.documentElement);
+	},
+	rmvVwlsFrm: function(lmnt)
+	{
+		if(!(lmnt instanceof HTMLElement))
+		{
+			return;
+		}
+		if(lmnt.children.length > 0)
+		{
+			for(let i in lmnt.children)
+			{
+				fckvwls.rmvVwlsFrm(lmnt.children[i]);
+			}
+		}
+		else if(lmnt.innerHTML && ["STYLE"].indexOf(lmnt.tagName.toUpperCase()) == -1)
+		{
+			lmnt.textContent = fckvwls.rmvVwlsFrmStrng(lmnt.textContent);
+		}
+	},
+	rmvVwlsFrmStrng: function(strng)
+	{
+		return strng.split("a").join("").split("e").join("").split("i").join("").split("o").join("").split("u").join("");
+	}
+};
